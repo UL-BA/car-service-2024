1d1ee03bcb0da62c05d0b0a774f727f1edda5afb
Sofiia <sofiiavoitkova@gmail.com>
2024-11-15 10:59:43 +0100

    feat[CS-11]: add Logo and Tab icon

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,16 @@
-import Navbar from './components/Navbar'
-import styles from './header.module.scss'
+import Navbar from "./components/Navbar";
+import styles from "./header.module.scss";
+import logo from "../../assets/logo.jpg";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
+        <div className={styles.logo}>
+          <img src={logo} alt="Logo" className={styles.logoImage} />
+        </div>
         <Navbar />
       </div>
     </header>
-  )
+  );
 }
