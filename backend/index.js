108981307e807837723d2e26876554eb97608237
Nara <mills.nargiz@gmail.com>
2024-12-08 20:14:19 +0100

    backend index update

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,12 @@ const port = process.env.PORT || 3000;
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', ''],
+    origin: [
+        'http://localhost:5173',
+        'https://car-service-front.vercel.app'  // Add your frontend Vercel URL
+    ],
     credentials: true
-}));
-
+}))
 // Root route
 app.get("/", (req, res) => {
     res.send("Road Ready Server is running!");
