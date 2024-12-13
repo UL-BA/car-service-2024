7717c50da8c3a1d18c2076c6fc5f267487a895e0
Nara <mills.nargiz@gmail.com>
2024-12-13 16:40:21 +0100

    PL;EN Language Switcher implementation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
 import { AuthProvider } from './contexts/AuthContext'
+import './i18n'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
