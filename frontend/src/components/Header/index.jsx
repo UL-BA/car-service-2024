37122424948b0bf3b1aa5e4848cfbec0a8e01287
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-12-18 12:24:24 +0100

    feat: add background color change on scroll to header

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,17 +1,35 @@
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import styles from "./header.module.scss";
 import logo from "../../assets/logo-removebg-preview.png";
 
-
 export default function Header() {
- return (
-   <header className={styles.header}>
-     <div className={styles.container}>
-       <div id="home" className={styles.logo}>
-         <img src={logo} alt="Logo" className={styles.logoImage} />
-       </div>
-       <Navbar />
-     </div>
-   </header>
- );
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <header className={`${styles.header} ${scrolled ? styles.scrolled : ""}`}>
+      <div className={styles.container}>
+        <div id="home" className={styles.logo}>
+          <img src={logo} alt="Logo" className={styles.logoImage} />
+        </div>
+        <Navbar />
+      </div>
+    </header>
+  );
 }
