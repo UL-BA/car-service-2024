3d6d1b31b1332d0b52686ef185df067243647df7
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-12-18 11:45:20 +0100

    feat: update header styles

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,27 +1,17 @@
-import React from "react";
-import { useNavigate } from "react-router-dom"; // Import useNavigate
 import Navbar from "./components/Navbar";
 import styles from "./header.module.scss";
-import logo from "../../assets/logo.jpg";
+import logo from "../../assets/logo-removebg-preview.png";
+
 
 export default function Header() {
-  const navigate = useNavigate(); // Initialize navigate
-
-  return (
-    <header className={styles.header}>
-      <div className={styles.container}>
-        <a
-          className={styles.logo}
-          onClick={(e) => {
-            e.preventDefault();
-            navigate("/");
-            window.scrollTo({ top: 0, behavior: "smooth" });
-          }}
-        >
-          <img src={logo} alt="Logo" className={styles.logoImage} />
-        </a>
-        <Navbar />
-      </div>
-    </header>
-  );
+ return (
+   <header className={styles.header}>
+     <div className={styles.container}>
+       <div id="home" className={styles.logo}>
+         <img src={logo} alt="Logo" className={styles.logoImage} />
+       </div>
+       <Navbar />
+     </div>
+   </header>
+ );
 }
