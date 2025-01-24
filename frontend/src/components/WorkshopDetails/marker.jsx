ff8bfdc396d58b41ceafb172f196ad6bbc72fd11
Nara <mills.nargiz@gmail.com>
2025-01-24 11:16:09 +0100

    Sprint 3

---
+++ frontend/src/components/WorkshopDetails/marker.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from "react";
+
+const AdvancedMarker = ({ map, position }) => {
+  const markerRef = useRef(null);
+
+  useEffect(() => {
+    if (!map || !position) return;
+    if (!markerRef.current) {
+      markerRef.current = new google.maps.Marker({
+        position,
+        map,
+        icon: {
+          url: "/src/assets/custom-marker.png",
+          scaledSize: new google.maps.Size(40, 40),
+        },
+      });
+    } else {
+      markerRef.current.setPosition(position);
+      markerRef.current.setMap(map);
+    }
+
+    return () => {
+      if (markerRef.current) {
+        markerRef.current.setMap(null);
+      }
+    };
+  }, [map, position]);
+
+  return null;
+};
+
+export default AdvancedMarker;
